378ed516894d201de50e17136b04ddc70437fc90
Sinan Karakaya <sinan.karakaya@epitech.eu>
2023-08-04 00:07:50 +0200

    feat: contact form

---
+++ app/[lng]/contact/page.tsx
@@ -0,0 +1,43 @@
+import { useTranslation } from '@/app/i18n'
+import ContactForm from '@/components/ContactForm'
+import { title } from '@/components/primitives'
+import { Card, CardBody, CardFooter, CardHeader } from '@nextui-org/card'
+import { Divider } from '@nextui-org/divider'
+import { Link } from '@nextui-org/link'
+
+export default async function ContactPage({ params: { lng } }: { params: { lng: string } }) {
+  const { t } = await useTranslation(lng, 'contact')
+
+  return (
+    <Card className='max-w-5xl w-full md:w-6/12'>
+      <CardHeader>
+        <h1 className={`${title({ size: 'sm' })}`}>{t('title')}</h1>
+      </CardHeader>
+      <Divider />
+      <CardBody>
+        <ContactForm lng={lng} />
+      </CardBody>
+      <Divider />
+      <CardFooter>
+        <div className='text-sm text-foreground-500'>
+          <p>My coordinates:</p>
+          <Link
+            href='mailto:sinan.karakaya@epitech.eu'
+            isExternal
+            showAnchorIcon
+          >
+            sinan.karakaya@epitech.eu
+          </Link>
+          <br />
+          <Link
+            href='tel:0781984783'
+            isExternal
+            showAnchorIcon
+          >
+            +33 7 81 98 47 83
+          </Link>
+        </div>
+      </CardFooter>
+    </Card>
+  )
+}
---
+++ components/ContactForm.tsx
@@ -0,0 +1,87 @@
+'use client'
+
+import { useState } from 'react'
+import { useTranslation } from '@/app/i18n/client'
+import { Button, Input, Spacer, Textarea } from '@nextui-org/react'
+
+export default function ContactForm({ lng }: { lng: string }) {
+  const { t } = useTranslation(lng, 'contact')
+  const [isSending, setIsSending] = useState(false)
+  const [isSent, setIsSent] = useState(false)
+  const [error, setError] = useState('')
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    message: '',
+  })
+
+  const sendMessage = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    setIsSending(true)
+
+    const res = await fetch('https://formspree.io/f/meqboqpk', {
+      method: 'POST',
+      body: JSON.stringify(formData),
+      headers: {
+        Accept: 'application/json',
+      },
+    })
+    if (!res.ok) {
+      setError(t('errorMessage'))
+    } else {
+      setIsSent(true)
+    }
+
+    setIsSending(false)
+  }
+
+  return (
+    <form className='grid grid-cols-1 md:grid-cols-2 gap-4' onSubmit={sendMessage}>
+      <Input
+        label={t('yourName')}
+        className='col-span-2'
+        value={formData.name}
+        onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+        isRequired
+      />
+      <Input
+        label={t('yourEmail')}
+        type='email'
+        value={formData.email}
+        onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+        isRequired
+      />
+      <Input
+        label={t('yourPhone')}
+        type='tel'
+        value={formData.phone}
+        onChange={(e) => setFormData({ ...formData, phone: e.target.value })}
+        isRequired
+      />
+      <Textarea
+        label={t('yourMessage')}
+        className='col-span-2'
+        isRequired
+        minRows={10}
+        value={formData.message}
+        onChange={(e) => setFormData({ ...formData, message: e.target.value })}
+      />
+      {isSent ? (
+        <p className='text-success-500 text-sm max-w-xs'>{t('sentMessage')}</p>
+      ) : error ? (
+        <p className='text-danger-500 text-sm'>{error}</p>
+      ) : (
+        <Spacer />
+      )}
+      <Button
+        variant='shadow'
+        color={isSent ? 'success' : 'primary'}
+        type='submit'
+        isLoading={isSending}
+      >
+        {t('send')}
+      </Button>
+    </form>
+  )
+}
