1ee1851094c3813e129b97fef665a235c57c9e7a
Sinan Karakaya <sinan.karakaya@epitech.eu>
2023-08-04 18:48:47 +0200

    fix: bad href

--- app/[lng]/about/page.tsx
+++ app/[lng]/about/page.tsx
@@ -68,7 +68,7 @@ export default async function About({ params: { lng } }: { params: { lng: string
               variant='shadow'
               color='primary'
               as={Link}
-              href='/about'
+              href='/projects'
             >
               {t('projects')}
             </Button>
