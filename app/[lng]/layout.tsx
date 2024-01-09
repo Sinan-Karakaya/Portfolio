734eb2c6d9e3587d8780e7735b9861e7c172cb35
Sinan <sinan.karakaya@epitech.eu>
2024-01-09 14:44:39 +0100

    added speed insights

--- app/[lng]/layout.tsx
+++ app/[lng]/layout.tsx
@@ -1,53 +1,47 @@
-import "@/styles/globals.css";
-import { Metadata } from "next";
-import { siteConfig } from "@/config/site";
-import { fontSans } from "@/config/fonts";
-import { Providers } from "./providers";
-import Navbar from "@/components/navbar";
-import clsx from "clsx";
-import { Analytics } from '@vercel/analytics/react';
+import '@/styles/globals.css'
+import { Metadata } from 'next'
+import { siteConfig } from '@/config/site'
+import { fontSans } from '@/config/fonts'
+import { Providers } from './providers'
+import Navbar from '@/components/navbar'
+import clsx from 'clsx'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 export const metadata: Metadata = {
-	title: {
-		default: siteConfig.name,
-		template: `%s - ${siteConfig.name}`,
-	},
-	description: siteConfig.description,
-	themeColor: [
-		{ media: "(prefers-color-scheme: light)", color: "white" },
-		{ media: "(prefers-color-scheme: dark)", color: "black" },
-	],
-	icons: {
-		icon: "/favicon.ico",
-		shortcut: "/favicon-16x16.png",
-		apple: "/apple-touch-icon.png",
-	},
-};
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  themeColor: [
+    { media: '(prefers-color-scheme: light)', color: 'white' },
+    { media: '(prefers-color-scheme: dark)', color: 'black' },
+  ],
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon-16x16.png',
+    apple: '/apple-touch-icon.png',
+  },
+}
 
-export default function RootLayout({
-	children,
-}: {
-	children: React.ReactNode;
-}) {
-	return (
-		<html lang="en" suppressHydrationWarning>
-			<head />
-			<body
-				className={clsx(
-					"min-h-screen bg-background font-sans antialiased",
-					fontSans.variable
-				)}
-			>
-				<Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
-					<div className="relative flex flex-col h-screen">
-						<Navbar />
-						<main className="flex justify-center items-start md:items-center h-full px-6">
-							{children}
-						</main>
-					</div>
-				</Providers>
-				<Analytics />
-			</body>
-		</html>
-	);
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html
+      lang='en'
+      suppressHydrationWarning
+    >
+      <head />
+      <body className={clsx('min-h-screen bg-background font-sans antialiased', fontSans.variable)}>
+        <Providers themeProps={{ attribute: 'class', defaultTheme: 'dark' }}>
+          <div className='relative flex flex-col h-screen'>
+            <Navbar />
+            <main className='flex justify-center items-start md:items-center h-full px-6'>{children}</main>
+          </div>
+        </Providers>
+        <SpeedInsights />
+        <Analytics />
+      </body>
+    </html>
+  )
 }
